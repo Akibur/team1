2b515eb03b6c84a9fea09aa2572faffa3495ed62
N4f33z <arashikun1994@gmail.com>
2017-04-30 15:07:34 +0600

    Add project files.

---
+++ team/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team
+{
+    class Class1
+    {
+        public Class1()
+            {
+                 Console.WriteLine("Heyaa add whatever you asdasdxzc likeasd");
+                 Console.ReadKey();
+
+            
+            }
+    }
+}
---
+++ team/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //add a class
+            Class1 ob = new Class1();
+        }
+    }
+}
